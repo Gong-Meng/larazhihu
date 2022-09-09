f68c7217bf03308bb13a586c31958744c49cf38b
Gong Meng <18588264128@163.com>
2022-09-09 14:43:39 +0800

    9.3 must-verify-email middleware

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/questions', 'QuestionsController@index');
 Route::post('/questions', 'QuestionsController@store')->name('questions.store');
--- tests/Feature/Questions/CreateQuestionsTest.php
+++ tests/Feature/Questions/CreateQuestionsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Questions;
 
 use App\Models\Category;
 use App\Models\Question;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -92,5 +93,17 @@ class CreateQuestionsTest extends TestCase
         $response = $this->post('/questions', $question->toArray());
         $response->assertRedirect();
         $response->assertSessionHasErrors('category_id');
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function authenticated_users_must_confirm_email_address_before_creating_questions()
+    {
+        $this->signIn(create(User::class, ['email_verified_at' => null]));
+        $question = make(Question::class);
+        $this->post('/questions', $question->toArray())
+            ->assertRedirect(route('verification.notice'));
     }
 }
