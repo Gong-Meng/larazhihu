2c4aa7c01ffb64c4300fca5d4cfed2a455b77963
Gong Meng <18588264128@163.com>
2022-09-09 10:44:10 +0800

    9.2 create question validation

--- tests/Feature/Questions/CreateQuestionsTest.php
+++ tests/Feature/Questions/CreateQuestionsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Questions;
 
+use App\Models\Category;
 use App\Models\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -34,5 +35,62 @@ class CreateQuestionsTest extends TestCase
         $this->assertCount(0, Question::all());
         $this->post('/questions', $question->toArray());
         $this->assertCount(1, Question::all());
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function title_is_required()
+    {
+        $this->signIn()->withExceptionHandling();
+        $response = $this->post('/questions', ['title' => null]);
+        $response->assertRedirect();
+        $response->assertSessionHasErrors('title');
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function content_is_required()
+    {
+        $this->signIn()->withExceptionHandling();
+        $question = make(Question::class)->toArray();
+        unset($question['content']);
+        $response = $this->post('/questions', $question);
+        $response->assertRedirect();
+        $response->assertSessionHasErrors('content');
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function category_id_is_required()
+    {
+        $this->signIn()->withExceptionHandling();
+
+        $question = make(Question::class)->toArray();
+        unset($question['category_id']);
+
+        $response =$this->post('/questions', $question);
+
+        $response->assertRedirect();
+        $response->assertSessionHasErrors('category_id');
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function category_id_is_existed()
+    {
+        $this->signIn()->withExceptionHandling();
+        create(Category::class, ['id' => 1]);
+        $question = make(Question::class, ['category_id' => 999]);
+        $response = $this->post('/questions', $question->toArray());
+        $response->assertRedirect();
+        $response->assertSessionHasErrors('category_id');
     }
 }
